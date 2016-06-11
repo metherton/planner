2fbd512e2a5a4f1d4b55697e943a8b6452015fbc
metherton <martinetherton63@gmail.com>
2016-06-11 23:27:35 +0200

    remove story from sprint

--- www/js/currentsprint.controller.js
+++ www/js/currentsprint.controller.js
@@ -6,9 +6,9 @@
 
   .controller('CurrentSprintCtrl', CurrentSprintCtrl);
 
-  CurrentSprintCtrl.$inject = ['stories', 'sprint', 'sprintFactory'];
+  CurrentSprintCtrl.$inject = ['stories', 'sprint', 'sprintFactory', '$scope'];
 
-  function CurrentSprintCtrl(stories, sprint, sprintFactory) {
+  function CurrentSprintCtrl(stories, sprint, sprintFactory, $scope) {
 
     var vm = this;
 
@@ -16,13 +16,33 @@
     vm.sprint = sprint;
 
     vm.onSwipeUp = handleSwipeUp;
+    vm.onSwipeDown = handleSwipeDown;
 
     function handleSwipeUp(storyId) {
       console.log('swipe up', storyId);
       vm.sprint.stories.push(storyId);
       vm.sprint.$update(function (response) {
         console.log(response.data);
+        $scope.$apply();
       });
+    }
+
+    function handleSwipeDown(storyId) {
+      console.log('swipe down', storyId);
+      var index;
+      for (var i = 0; i < vm.sprint.stories.length; i++) {
+        if (vm.sprint.stories[i]._id === storyId) {
+          index =  i;
+          break;
+        }
+      }
+      if (index !== undefined) {
+        vm.sprint.stories.splice(index);
+        vm.sprint.$update(function (response) {
+          console.log(response.data);
+          $scope.$apply();
+        });
+      }
     }
   }
 })();
